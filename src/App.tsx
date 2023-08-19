7ba277ac60572b7bea53415044a3a6f81c9489c7
Mohammed Ibrahim <agentmame@gmail.com>
2023-08-19 16:34:04 +0300

    Added tailwind to project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import "./index.css";
 
 function App() {
-
   return (
     <>
-    <h1>Hello World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
