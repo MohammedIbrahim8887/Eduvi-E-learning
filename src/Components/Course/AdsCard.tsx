754337fed3b254d21761d2633e938231c8b563b7
Daniel Asfaw <asfawdaniel779@gmail.com>
2023-08-21 17:15:24 -0500

    made adds card

---
+++ src/Components/Course/AdsCard.tsx
@@ -0,0 +1,10 @@
+
+const AdsCard = () => {
+    return (
+      <div className=" flex justify-center align-middle w-[100px] h-[100px] overflow-hidden p-2">
+        <img src="public/vite.svg" alt="Add" className=" w-full h-full object-cover"/>
+      </div>
+    )
+  }
+  
+  export default AdsCard
