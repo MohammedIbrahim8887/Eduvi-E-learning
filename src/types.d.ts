95b89f499daffa7e535875a1c039cf939988c749
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-08-22 16:46:17 +0300

    Create types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export interface CourseCardProps {
+    image?: string;
+    rating?: number;
+    description?: string;
+    videos?: number;
+    duration?: number;
+    name?: string;
+    students?: number;
+  }
+
