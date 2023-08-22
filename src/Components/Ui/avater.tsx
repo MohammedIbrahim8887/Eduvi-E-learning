d414696522daa789fb60a58e497218de9bacdcb0
seid yesuf <seidyesuf750@gmail.com>
2023-08-22 16:08:38 -0700

    ui components such as avater link logo popover select

---
+++ src/Components/Ui/avater.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function avater() {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <div className='flex'>
+      My Account
+      <Button aria-describedby={id} variant="text" onClick={handleClick}>
+      <div className='flex'> <span className='ml-3 hover:shadow-lg cursor-pointer'  >
+      <img className='w-5 h-5' src="/vite.svg" alt="s" /> </span></div>
+      </Button>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+      </Popover>
+    </div>
+  );
+}
+
