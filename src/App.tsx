d408b156b97ae1deca1fef8424007c68be75f0b0
inasw <inaswendem@gmail.com>
2023-08-25 15:52:51 +0300

    Subscriptionbox Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./Utils/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import SubscriptionBox from "./Components/Common/SubscriptionBox";
 // only add provider wrappers here
 
 const queryClient = new QueryClient();
@@ -12,6 +12,7 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+          <SubscriptionBox/>
       </QueryClientProvider>
     </>
   );
