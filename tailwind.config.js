0614d26e52e2e454359b502f31f7beee500c3a0b
Mihretthe <115806991+Mihretthe@users.noreply.github.com>
2023-08-26 12:50:49 +0300

    Update tailwind.config.js additional configs both in spacing and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ export default {
         'card-height': '418px',        
         'rating-width' : '63px',
         'rating-height' : '25px',  
-        'image-height' : '236px',  
+        'image-height' : '236px',
+         'quote-width' : "700px",  
+        'quote-height' : "135px",  
+        'cat-align' : "1008px",
+        'cat-pos' : "10%",
+        'catb-pos' : "5%",
+        'rating-bottom' : '40%',
+        'b-width' : '1300px',
 
       },
       colors: {
@@ -17,6 +24,9 @@ export default {
         'rating-color' : '#1C1E53',
         'desc-color' : '#282938',
         'icon-color' : '#000000',
+        'quote-title-color' : ' #0A033C',
+        'quote-color': '#5D5A6F',
+        'list-back' : '#fff',
       }
     },
   },
