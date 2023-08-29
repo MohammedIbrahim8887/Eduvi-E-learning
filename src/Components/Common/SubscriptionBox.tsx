13bac0bcf5576e44775fe1968ae18d44aa42ebf8
inasw <inaswendem@gmail.com>
2023-08-29 12:32:57 +0300

    subscribe

--- src/Components/Common/SubscriptionBox.tsx
+++ src/Components/Common/SubscriptionBox.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import {useState} from 'react';
+import {useForm,SubmitHandler} from 'react-hook-form';
+
+type FormValues = {
+  email:string;
+}
 
 const SubscriptionBox: React.FC = () => {
-  const [inputClicked, setInputClicked] = useState(false);
 
-  const handleInputClick = () => {
-    setInputClicked(true);
-  };
-
-  const handleInputBlur = () => {
-    setInputClicked(false);
-  };
-
+    const {register,handleSubmit, formState: {errors}} = useForm<FormValues>();
+    const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
+   
+  
   return (
     <div className="w-full max-w-3xl mx-auto px-8 lg:px-16 my-8 h-80  bg-[#0A033C]
      text-white rounded-lg flex flex-col items-center justify-center">
@@ -27,31 +26,53 @@ const SubscriptionBox: React.FC = () => {
           20k+ students daily learn with Eduvi, subscribe for new courses.
         </p>
       </div>
-      <div className="flex flex-row justify-center mt-8">
-        <input
-          type="email"
-          className={`w-full sm:w-auto h-10 px-4 py-2 rounded-l-lg rounded-bl-lg border bg-[#0A033C]
-          bg-opacity-0 
-          
-          mb-2 sm:mb-0`}
-          placeholder="enter your email"
-          style={{ fontStyle: "italic", overflow: "hidden",
-          background: "#6B6B6B", }}
-          onClick={handleInputClick}
-          onBlur={handleInputBlur}
-        />
-        <button className="w-full text-small px-4 sm:w-auto h-10 bg-indigo-700 text-white rounded-r-lg rounded-br-lg" >
-          Subscribe
-        </button>
-      </div>
-       
+
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-row justify-center mt-8">
+          <input type="email"
+            {...register("email", {required: "Email is required"})}
+            className="w-full sm:w-auto h-10 px-4 py-2 rounded-l-lg rounded-bl-lg 
+            mb-2 sm:mb-0  placeholder:text-black-400 placeholder:italic 
+            bg-white bg-opacity-10 focus:bg-opacity-20 duration-150"
+            placeholder="enter your email"
+             />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p> }
+
+            <button 
+              type="submit" 
+              className="bg-blue-500 p-2 rounded hover:bg-blue-600">
+              Subscribe
+            </button>
+        </div>
+
+      </form>
     </div>
   );
 };
 
 export default SubscriptionBox;
 
+{/* <form action="" method="post">
+      <div className="flex flex-row justify-center mt-8">
 
+        <input
+          type="email"
+          placeholder="enter your email"
+          className="w-full sm:w-auto h-10 px-4 py-2 rounded-l-lg rounded-bl-lg 
+           mb-2 sm:mb-0  placeholder:text-black-400 placeholder:italic 
+           bg-white bg-opacity-10 focus:bg-opacity-20 duration-150"/>
+          
+          
+         
+          
+         
+         
+        
+        <button className="w-full text-small px-4 sm:w-auto h-10 bg-indigo-700 text-white rounded-r-lg rounded-br-lg" >
+          Subscribe
+        </button>
+      </div>
+      </form> */}
 
 
 
