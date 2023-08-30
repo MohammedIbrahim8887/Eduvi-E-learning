86e5295754bf23302749ea9ea4d947488f9af11e
seid yesuf <seidyesuf750@gmail.com>
2023-08-30 07:32:54 -0700

    renaming fiile name

--- src/Utils/routes.tsx
+++ src/Utils/routes.tsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import Home from "../Pages/Home";
 import Error404 from "../Pages/ErrorPages/404";
-import AllCourses from "../Pages/allcourses";
+import CoursesDetails from "../Pages/CoursesDetails";
 //Add your routes here
 
 const router = createBrowserRouter(
@@ -28,7 +28,7 @@ const router = createBrowserRouter(
       <Route path="/privacy" element={''} />
       <Route path="/provision" element={''} />
       <Route path="/provision" element={''} />
-      <Route path="/:id" element={<AllCourses />} />
+      <Route path="/:id" element={<CoursesDetails />} />
     
     </Route >
   )
